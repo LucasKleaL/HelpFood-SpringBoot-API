9a8b65fd0ebf9909eaae4ca268ce342f383cca08
LucasKleaL <63320965+LucasKleaL@users.noreply.github.com>
2022-10-12 17:29:40 -0300

    Adicionado CRUD de User
    -Adicionado os endpoints no UserController.
    -Adicionado o entity model de User.
    -Adicionado UserRepository conectando ao jpa.
    -Adicionado métodos do CRUD no UserService.

--- UserService/src/main/java/com/example/userservice/user/repository/UserRepository.java
+++ UserService/src/main/java/com/example/userservice/user/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.userservice.user.repository;
 
-public class UserRepository {
+import com.example.userservice.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
--- UserService/src/main/java/com/example/userservice/user/service/UserService.java
+++ UserService/src/main/java/com/example/userservice/user/service/UserService.java
@@ -1,4 +1,42 @@
 package com.example.userservice.user.service;
 
+import com.example.userservice.user.entity.User;
+import com.example.userservice.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) { //exeçao
+        if (user.getName() == null || user.getName().equals("") || user.getName().length() > 55) {
+            //throw exception
+        }
+        return userRepository.save(user);
+    }
+
+    public List<User> getAll() {
+        List<User> users = userRepository.findAll();
+        /* buscar doações relacionadas ao usuário
+        for (User user : users) {
+
+        }
+        */
+
+        return users;
+    }
+
+    public User findById(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    public void delete(Integer id) {
+        userRepository.deleteById(id);
+    }
+
 }
