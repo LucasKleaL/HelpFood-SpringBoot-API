9928c5f50301d4bbda2f68c14303e11287cb51cb
LucasKleaL <63320965+LucasKleaL@users.noreply.github.com>
2022-11-01 18:50:11 -0300

    Adicionado SwaggerUi ao UserService

--- UserService/src/main/java/com/example/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,8 +1,18 @@
 package com.example.userservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(title = "API de usuários para doações", version = "3.0.1", description = "Helpfood"),
+        servers = {
+                @Server(url = "http://localhost:8080"),
+                @Server(url = "http://localhost:8080/user"),
+        }
+)
 @SpringBootApplication
 public class UserServiceApplication {
 
