46106911438cbb62d4271b3ab737e117cc319003
LucasKleaL <lucaskleal222@outlook.com>
2022-11-19 17:21:46 -0300

    Refatorado validações de User
    -Adicionado validação de email unique.
    -Adicionado validação de tamanho de nome.
    -Adicionado validação de tamanho de CNPJ.
    -Adicionado atribuição de valor padrão para emailVerified e allowed.
    -Corrigido código dos erros de retorno na validação de User

--- UserService/src/main/java/com/helpfood/userservice/user/service/UserService.java
+++ UserService/src/main/java/com/helpfood/userservice/user/service/UserService.java
@@ -26,17 +26,38 @@ public class UserService {
     private FeignDonation feignDonation;
 
     public User save(User user) throws MessageException {
-        if (user.getName() == null || user.getName().equals("") || user.getName().length() > 55) {
-            throw new MessageException("USER_ERR001", "Invalid user data (name).");
+        // Name validation
+        if (user.getName() == null || user.getName().equals("")) {
+            throw new MessageException("USER_ERR001", "Invalid user data (Name cannot be null).");
         }
+        if (user.getName().length() > 55 || user.getName().length() < 3) {
+            throw new MessageException("USER_ERR001", "Invalid user data (Name must be bigger than 2 and smaller than 55 char).");
+        }
+        // Email validation
         if (user.getEmail() == null || user.getEmail().equals("")) {
-            throw new MessageException("USER_ERR002", "Invalid user data (email).");
+            throw new MessageException("USER_ERR001", "Invalid user data (Email cannot be null).");
         }
+        if (userRepository.findFirstByEmail(user.getEmail()) != null) {
+            throw new MessageException("USER_ERR002", "Email already registered.");
+        }
+        // Cnpj validation
         if (user.getCnpj() == null || user.getCnpj().equals("")) {
-            throw new MessageException("USER_ERR003", "Invalid user data (cnpj)");
+            throw new MessageException("USER_ERR001", "Invalid user data (CNPJ cannot be null)");
         }
+        if (user.getCnpj().length() != 14) {
+            throw new MessageException("USER_ERR001", "Invalid user data (CNPJ must be 14 numbers).");
+        }
+        // Role validation
         if (user.getRole() == null || user.getRole().equals("")) {
-            throw new MessageException("USER_ERR004", "Invalid user data (role).");
+            throw new MessageException("USER_ERR001", "Invalid user data (Role cannot be null).");
+        }
+        // Email verified attribution
+        if (user.getEmailVerified() == null) {
+            user.setEmailVerified(false);
+        }
+        // Allowed attribution
+        if (user.getAllowed() == null) {
+            user.setAllowed(true);
         }
 
         return userRepository.save(user);
